ab1f4d8e62ae35e684daf83e7c883ab13d1b8e89
RenanTormen <renancvl@gmail.com>
2019-10-24 10:38:32 -0300

    Capitulo inicial sobre Rotas usando ActivatedRoute

--- src/app/detalhes-produto/detalhes-produto.component.ts
+++ src/app/detalhes-produto/detalhes-produto.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { products } from '../products';
 
 @Component({
-  selector: 'app-detalhes-produto',
-  templateUrl: './detalhes-produto.component.html',
-  styleUrls: ['./detalhes-produto.component.css']
+  selector: "app-detalhes-produto",
+  templateUrl: "./detalhes-produto.component.html",
+  styleUrls: ["./detalhes-produto.component.css"]
 })
 export class DetalhesProdutoComponent implements OnInit {
+  
+  produto;
 
-  constructor() { }
+  constructor(
+    private rota: ActivatedRoute,
+  ) {}
 
   ngOnInit() {
+    this.rota.paramMap.subscribe(parametros =>{
+      this.produto = products[parametros.get('idProduto')];
+    });
   }
-
 }
